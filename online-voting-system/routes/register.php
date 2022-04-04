2f2cbe28f6dd6c6a55647c2e3e2aaae0d0fe6b5d
Datta Patil <dattashitule2910@gmail.com>
2022-04-04 22:14:13 +0530

    akash

---
+++ online-voting-system/routes/register.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Online voting system - Registratrion</title>
+        <link rel="stylesheet" href="../css/stylesheet.css">
+    </head>
+    <style>
+        #h3 {
+            position:absolute;right:100;bottom:100;
+        }
+        </style>
+
+    <body>
+        <center>
+            <div id="headerSection">
+            <h1>Online Voting System</h1>  
+            </div>
+            <hr>
+
+            <h2>Registration</h2>
+                <form action="../api/register.php" method="POST" enctype="multipart/form-data">
+                    <input type="text" name="name" placeholder="Name" required>&nbsp
+                    <input type="number" name="mob" placeholder="Mobile" required><br><br>
+                    <input type="password" name="pass" placeholder="Password" required>&nbsp
+                    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>
+                    <input style="width: 31%" type="text" name="add" placeholder="Address" required><br><br>
+                    <div id="upload" style="width: 30%">
+                        Upload image: <input type="file" id="profile" name="image" required>
+                    </div><br>
+                    <div id="upload" style="width: 30%">
+                        Select your role:
+                        <select name="role">
+                            <option value="1">Voter</option>
+                            <option value="2">Group</option>
+                        </select><br>                   
+                    </div><br>
+                    <button id="loginbtn" type="submit" name="registerbtn">Register</button><br><br>
+                    Already user? <a href="../">Login here</a>
+               </form>
+               <h3> made by AKASH GHOLVE, </h3>
+               <h3> UMESH UBLAE , </h3>
+               <h3> Datta Shitule </h3>
+            </center>
+    </body>
+</html>
